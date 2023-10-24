aa9a2e9360d8506115f38f8b4643943678f93154
Yumi Izumi <me@yumiizumi.com>
2023-10-24 10:50:30 -0400

    Add elapsed time

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use chrono::Local;
 use discord_rich_presence::{
-    activity::{Activity, Assets},
+    activity::{Activity, Assets, Timestamps},
     DiscordIpc, DiscordIpcClient,
 };
-use std::{process::Command, thread, time::Duration};
+use std::{
+    process::Command,
+    thread,
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
 
 const WAIT_TIME: u64 = 5;
 
@@ -45,6 +49,8 @@ fn discord_rpc() -> Result<(), Box<dyn std::error::Error>> {
                 Local::now().format("%Y-%m-%d %H:%M:%S")
             );
             let mut xcode_is_running = check_xcode()?;
+            let mut started_at = Timestamps::new().start(current_time());
+            let mut project_before = String::from("");
 
             while xcode_is_running {
                 let file = current_file()?;
@@ -52,9 +58,22 @@ fn discord_rpc() -> Result<(), Box<dyn std::error::Error>> {
                 let file_extension = (file.split('.').collect::<Vec<&str>>().last().unwrap_or(&""))
                     .trim()
                     .to_string();
+                let project_is_empty = project.is_empty();
+                if !project_before.eq(&project) {
+                    started_at = Timestamps::new().start(current_time());
+                    project_before = project.clone();
+                }
 
-                let details = format!("Working on {}", file);
-                let state = format!("in {}", project);
+                let details = if project_is_empty {
+                    String::from("Idle")
+                } else {
+                    format!("Working on {}", file)
+                };
+                let state = if project_is_empty {
+                    String::from("Idle")
+                } else {
+                    format!("in {}", project)
+                };
 
                 let keys = match file_extension.as_str() {
                     "swift" => ("Swift", "swift"),
@@ -68,7 +87,8 @@ fn discord_rpc() -> Result<(), Box<dyn std::error::Error>> {
                 let activity = Activity::new()
                     .details(&details)
                     .state(&state)
-                    .assets(Assets::new().large_image(keys.1).large_text(keys.0));
+                    .assets(Assets::new().large_image(keys.1).large_text(keys.0))
+                    .timestamps(started_at.clone());
                 client.set_activity(activity)?;
 
                 thread::sleep(Duration::from_secs(WAIT_TIME));
@@ -122,6 +142,9 @@ fn current_project() -> Result<String, Box<dyn std::error::Error>> {
     )?
     .trim()
     .to_string();
+    if project == "missing value" {
+        return Ok(String::new());
+    }
     if project.starts_with("workspace document ") {
         return Ok(project.replace("workspace document ", ""));
     }
@@ -135,4 +158,11 @@ fn run_osascript(script: &str) -> Result<String, Box<dyn std::error::Error>> {
         .output()
         .expect("Failed to execute command");
     Ok(String::from_utf8_lossy(&output.stdout).to_string())
+}
+
+fn current_time() -> i64 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Failed to obtain current time")
+        .as_secs() as i64
 }
